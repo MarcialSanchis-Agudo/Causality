ccbc6751a5b734c919469c0d3d75f6779bb019a6
cberbarbanoj <cbbarbanoj@unizar.es>
2025-03-07 14:56:33 +0100

    Updated training script

---
+++ VAE/SCRIPTS/VAE/OBSTACLE_3D/VAE_3D_TRY.py
@@ -0,0 +1,202 @@
+import pyLOM
+import datetime
+import numpy as np
+import os
+import time
+import matplotlib as plt
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, LearningRateMonitor
+from torch.utils.data import Dataset, random_split, DataLoader
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
+from src.dataset import LazyH5Dataset
+
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+@rank_zero_only
+def create_results_folder(RESUDIR, echo=True):
+    if not os.path.exists(RESUDIR):
+        os.makedirs(RESUDIR)
+        if echo:
+            print(f"Folder created: {RESUDIR}")
+    else:
+        if echo:
+            print(f"Folder already exists: {RESUDIR}")
+
+# Specify autoencoder parameters
+ptrain        = 0.8
+pvali         = 0.2
+batch_size    = 32
+nepochs       = 1500
+nlayers       = 4
+channels      = 64
+latent_dim    = 4
+beta          = 1e-2
+beta_wmup     = 500
+beta_start    = 50
+kernel_size   = 4
+nlinear       = 256
+padding       = 1
+reduction     = 'mean'
+activations   = [pyLOM.NN.silu()] * 8
+learning_rate = 1e-5
+lr_decay      = 0.999
+batch_norm    = True
+vae           = True
+
+# Load pyLOM dataset and set up results output
+
+DATAFILE = ['/mimer/NOBACKUP/groups/kthmech/carlos/Datasets_3D/Obstacle_VAE/obstacle_3D_8.h5']
+RESUDIR = f"vae_beta_{beta}_ld_{latent_dim}_batch_{batch_size}_nlinear_{nlinear}_test_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+VARIABLE_1 = 'VELOX'
+VARIABLE_2 = 'VELOY'
+VARIABLE_3 = 'VELOZ'
+variables = (VARIABLE_1, VARIABLE_2, VARIABLE_3)
+create_results_folder(RESUDIR)
+name = f"VAE_3D_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+lazy_dataset = LazyH5Dataset(DATAFILE, variable_names=variables)
+print("Lazy dataset created.")
+print("Mesh shape:", lazy_dataset.mesh_shape)
+print("Number of channels:", lazy_dataset.num_channels)
+if lazy_dataset.time is not None:
+    print("Time vector shape:", lazy_dataset.time.shape)
+else:
+    print("No time vector found.")
+
+# Test one sample
+sample0 = lazy_dataset[0]
+print("First sample shape:", sample0.shape)
+
+start = time.time()
+for i in range(5):
+    _ = lazy_dataset[i]
+print(f"Avg Sample Load Time: {(time.time() - start) / 5:.3f}s per sample")
+
+total_samples = len(lazy_dataset)
+train_size = int(ptrain * total_samples)
+valid_size = total_samples - train_size
+td_train, td_valid = random_split(lazy_dataset, [train_size, valid_size])
+
+print(f"Train split size: {len(td_train)}")
+print(f"Val split size: {len(td_valid)}")
+
+# DataLoaders (num_workers se puede ajustar; en este ejemplo 1)
+def worker_init_fn(worker_id):
+    print(f"Worker {worker_id} started.")
+    global file_cache
+    file_cache = {}
+
+num_workers = min(2, os.cpu_count() // torch.cuda.device_count())
+prefetch_factor = 16
+
+trloader = DataLoader(
+    td_train,
+    batch_size=batch_size,
+    num_workers=num_workers,              
+    shuffle=True,
+    worker_init_fn=worker_init_fn,
+    prefetch_factor=prefetch_factor,    
+    pin_memory=True,          
+    persistent_workers=True    
+)
+
+valoader = DataLoader(
+    td_valid,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    worker_init_fn=worker_init_fn,
+    prefetch_factor=prefetch_factor,
+    pin_memory=True,
+    persistent_workers=True
+)
+
+# Verificación de algunos batches
+for batch_idx, batch in enumerate(trloader):
+    print(f"Batch {batch_idx + 1}:")
+    print("Tamaño del lote:", batch.shape)
+    if batch_idx == 2:
+        break
+
+# nx, ny, nz = data_module.nx, data_module.ny, data_module.nz
+# num_channels = data_module.nn_dataset.num_channels
+# Set the trainer for the variational autoencoder
+nx, ny, nz = lazy_dataset.mesh_shape
+num_channels = lazy_dataset.num_channels
+
+
+betasch = pyLOM.NN.betaLinearScheduler(0., beta, beta_start, beta_wmup)
+encoder = pyLOM.NN.Encoder3D(nlayers, latent_dim, nx, ny, nz, num_channels, channels, kernel_size, padding, activations, nlinear, batch_norm, stride=2, dropout=0, vae=vae)
+decoder = pyLOM.NN.Decoder3D(nlayers, latent_dim, nx, ny, nz, num_channels, channels, kernel_size, padding, activations, nlinear, batch_norm, stride=2, dropout=0)
+
+VAE = pyLOM.NN.VariationalAutoencoder_PL(
+    latent_dim     = latent_dim,
+    in_shape       = (nx, ny, nz),
+    input_channels = num_channels,
+    betasch        = betasch,
+    encoder        = encoder,
+    decoder        = decoder,
+    reduction      = reduction,
+    learning_rate  = learning_rate,
+    lr_decay       = lr_decay
+)
+print(VAE)
+
+# Create WandB Logger
+wandb_logger = WandbLogger(name=name, project='VAE_3D')
+wandb_logger.watch(VAE)
+
+early_stop_callback = EarlyStopping(
+    monitor  = 'val_loss',
+    patience = 20,
+    mode     = 'min'
+)
+
+checkpoint_callback = ModelCheckpoint(
+    monitor    = 'val_loss',
+    dirpath    = RESUDIR,
+    filename   = 'VAE_{epoch:02d}_{val_loss:.2f}',
+    save_top_k = 5,
+    mode       = 'min'
+)
+
+lr_monitor = LearningRateMonitor(
+    logging_interval = 'epoch'
+)
+
+# Train using multiple GPUs
+gpus = -1 if torch.cuda.is_available() else 0
+trainer = Trainer(
+    logger            = wandb_logger,
+    max_epochs        = nepochs,
+    devices           = gpus,
+    num_nodes         = 1,
+    accelerator       = 'auto',
+    strategy          = 'ddp_find_unused_parameters_false',
+    callbacks         = [checkpoint_callback, early_stop_callback, lr_monitor],
+    gradient_clip_val = 0.5,
+    precision         = 'bf16-mixed' if torch.cuda.is_available() else '32'
+)
+
+trainer.fit(model=VAE, train_dataloaders=trloader, val_dataloaders=valoader)
+
+
+"""
+## Reconstruct dataset and compute accuracy
+rec = VAE.reconstruct(dataset)  # Devuelve (input channels, nx*ny, time)
+corr, detR = VAE.correlation(dataset)
+recdtset = pyLOM.NN.Dataset((rec,), (nx, ny, nz))
+recdtset.pad((nx, ny, nz))
+dataset.pad((nx, ny, nz))
+time = dataset.get_variable('time')
+dataset.add_field('urec',1,recdtset[:,0,:,:].numpy().reshape((len(time),nx*ny*nz)).T)
+dataset.add_field('utra',1,recdtset[:,0,:,:].numpy().reshape((len(time),nx*ny*nz)).T)
+# pyLOM.io.pv_writer(m,d,'reco',basedir=RESUDIR,instants=np.arange(time.shape[0],dtype=np.int32),times=time,vars=['urec','VELOX','utra'],fmt='vtkh5')
+# pyLOM.NN.plotSnapshot(m,d,vars=['urec'],instant=0,component=0,cmap='jet')
+# pyLOM.NN.plotSnapshot(m,d,vars=['utra'],instant=0,component=0,cmap='jet')
+
+pyLOM.cr_info()
+
+"""
